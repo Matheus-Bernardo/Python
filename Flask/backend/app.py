0a0859b007ae0abc12bf9ae9d131d4e28aaec32e
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2023-11-01 18:20:42 -0300

    funcional

--- Flask/backend/app.py
+++ Flask/backend/app.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-from flask_cors import CORS
+
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/api', methods=["POST"])
 def calcula_imc():
